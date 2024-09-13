7798a8be87a9953e3af599eac458159c395b33ff
KS-777 <butageme4649@gmail.com>
2024-09-13 14:26:52 +0900

    [remove]ping.js removed
    ping.jsを消した。pingが動かないから。

--- src/index.js
+++ src/index.js
@@ -51,19 +51,6 @@ client.on(Events.InteractionCreate, async interaction => {
             }
         }
     }
-    if (interaction.commandName === pingFile.data.name) {
-        try {
-            await pingFile.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'コマンド実行時にエラーになりました。', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'コマンド実行時にエラーになりました。', ephemeral: true });
-            }
-        }
-    };
 });
-
 //BOTの起動 tokenは.envに記述しておく
 client.login(token)
