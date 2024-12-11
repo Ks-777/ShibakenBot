2f86a2e048bb025ac2cf526764d53b789fa93dd6
KS-777 <butageme4649@gmail.com>
2024-12-11 18:00:03 +0900

    [add] /tyouhan embed
    /tyouhanにembedを追加

--- src/commands/tyouhan.js
+++ src/commands/tyouhan.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, Colors } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const token_unb = process.env.token_unb;
 
 module.exports = {
@@ -27,28 +27,7 @@ module.exports = {
         const randnum_2 = Math.floor(Math.random() * 6) + 1;
         const randnum = randnum_1 + randnum_2;
         const userId = interaction.user.id;
-        const userTag = interaction.user.tag;
-        const url = 'https://unbelievaboat.com/api/v1/guilds/1250416661522153553/users/1223810206333407234';
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: token_unb
-            }
-        };
         
-        // fetchメソッドを使用
-        const response = await fetch(url, options);
-        const data = await response.json();
-        const userCash = data.cash;
-
-        console.log(`${userId} has ${userCash}`);
-
-        if (userCash <= kakekin_tyouhan) {
-            await interaction.reply({ content: `エラー:\nあなたは、${userCash}チー持っていますが、\n${kakekin_tyouhan}よりも等しいか少ないです。(チーが足りません)`, ephemeral: true });
-            return;
-        }
-
         function kake_remove() {
             const url = `https://unbelievaboat.com/api/v1/guilds/1250416661522153553/users/${userId}`;
             const kakekin_tyouhan_mainasu = -1 * kakekin_tyouhan;
@@ -87,52 +66,21 @@ module.exports = {
         }
 
         const randnum_th = randnum % 2 === 0 ? 0 : 1;
-        const kakekin_tyouhan_2bai = kakekin_tyouhan * 2;
+
         if (randnum_th === 0) {
             if (tyou_han === '0') {
                 await kake_fuyo();
-                await interaction.reply({embeds:[{
-                    color: Colors.Green,
-                    author:{
-                        name: userTag,
-                    },
-                    title: "勝ったぜ。(丁-丁)",
-                    description: `あなたは丁を選択し、サイコロで${randnum} (${randnum_1}+${randnum_2})が出て「丁」(偶数)だったため、\n${kakekin_tyouhan_2bai}(実際増えたのは${kakekin_tyouhan})増えました。`
-                }],
-                });
+                await interaction.reply({ content: `あなたは丁を選択し、サイコロは${randnum}(${randnum_1}+${randnum_2})(丁)が出たので成功しました！\n${kakekin_tyouhan}チーが二倍になって付与されました！` });
             } else {
-                await interaction.reply({embeds:[{
-                    color: Colors.Red,
-                    author:{
-                        name: userTag,
-                    },
-                    title: "負けちゃった...(半-丁)",
-                    description: `あなたは半を選択し、サイコロで${randnum} (${randnum_1}+${randnum_2})が出て「丁」(偶数)だったため、\n${kakekin_tyouhan}ﾁｰ負けました。`
-                }],
-                });
+                await interaction.reply({ content: `あなたは半を選択し、サイコロは${randnum}(${randnum_1}+${randnum_2})(丁)が出たので失敗しました。\n${kakekin_tyouhan}チーは没収されました。` });
             }
         } else {
             if (tyou_han === '1') {
                 await kake_fuyo();
-                await interaction.reply({embeds:[{
-                    color: Colors.Green,
-                    author:{
-                        name: userTag,
-                    },
-                    title: "勝ったぜ。(半-半)",
-                    description: `あなたは半を選択し、サイコロで${randnum} (${randnum_1}+${randnum_2})が出て「半」(奇数)だったため、\n${kakekin_tyouhan_2bai}(実際増えたのは${kakekin_tyouhan})増えました。`
-                }],
-                });
+                await interaction.reply({ content: `あなたは半を選択し、サイコロは${randnum}(${randnum_1}+${randnum_2})(半)が出たので成功しました！\n${kakekin_tyouhan}チーが二倍になって付与されました！` });
             } else {
-                await interaction.reply({embeds:[{
-                    color: Colors.Red,
-                    author:{
-                        name: userTag,
-                    },
-                    title: "負けちゃった...(丁-半)",
-                    description: `あなたは丁を選択し、サイコロで${randnum} (${randnum_1}+${randnum_2})が出て「半」(奇数)だったため、\n${kakekin_tyouhan}ﾁｰ負けました。`
-                }],
-                });
+                await interaction.reply({ content: `あなたは丁を選択し、サイコロは${randnum}(${randnum_1}+${randnum_2})(半)が出たので失敗しました。\n${kakekin_tyouhan}チーは没収されました。` });
+            }
         }
     }
-},}
+};
